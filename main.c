8108e83096047d24b2707910bb43a30899edaef9
AyeletKat <ayeletk03@gmail.com>
2024-02-26 14:58:32 +0200

    commit

--- main.c
+++ main.c
@@ -6,97 +6,78 @@
 int main() {
     StrList* myList = StrList_alloc();
     int choice;
-    //int size;//trying dinamiic array size for each word
-    char data[100]; // מחרוזת לקליטת נתונים מהמשתמש
+    char data[200]; // מחרוזת לקליטת נתונים מהמשתמש
 
     while (1) {
         if (scanf("%d", &choice) != 1){
             printf("Invalid input.");
-            break;
+            while (getchar() != '\n'); // Clear input buffer
+            continue;
         }
-//TODO: MAKE STRING MAX SIZE INSTEAD OF MAX SIZE DYNAMYC SIZE WITH GETLINE AND STRTOK
         switch(choice) {
             
             case 1:
-                char a;
-                getchar();//i have added trying to fix a bug
-                scanf("%c", &a);
-                    //printf("הכנס מספר מחרוזות להכנס לרשימה: ");
-                    int numStrings;
-                    scanf("%d", &numStrings);
-                    getchar(); // Consume newline character from the previous scanf
+                int numStrings;//number of strings to be entered
+                scanf("%d", &numStrings);
+                getchar(); // Consume newline character from the previous scanf
 
-                    //printf("הכנס את המחרוזות (הקלד 0 כדי לסיים):\n");
-                    for (int i = 0; i < numStrings; ++i) {
-                        scanf("%s", data);
-                        if (strcmp(data, "0") == 0) {
-                        break; // Exit loop if user enters "0"
-                        }
+                //if suddenly entered 0 to end program
+                for (int i = 0; i < numStrings; ++i) {
+                    scanf("%s", data);
                     StrList_insertLast(myList, data);
-                    }
+                }
                 break;
 
             case 2:
-                //printf("הכנס אינדקס: ");
-                int index;
+                int index;//index to enter data at
                 scanf("%d", &index);
-                //printf("הכנס מחרוזת: ");
-                scanf("%s", data);
+                scanf("%s", data);//string to be added at this index
                 StrList_insertAt(myList, data, index);
                 break;
             case 3:
-                //printf("הרשימה היא:\n");
-                StrList_print(myList);
+                StrList_print(myList);//printing the list
                 break;
             case 4:
-                printf("%zu\n", StrList_size(myList));
+                printf("%zu\n", StrList_size(myList));//printing list's size, how many strings it contains.
                 break;
             case 5:
-                //printf("הכנס אינדקס: ");
-                scanf("%d", &index);
+                scanf("%d", &index);//index of list's content to print
                 StrList_printAt(myList, index);
                 break;
             case 6:
-                printf("%d\n", StrList_printLen(myList));
+                printf("%d\n", StrList_printLen(myList));//prints the amount of chars in list
                 break;
             case 7:
-                //printf("הכנס מחרוזת לחיפוש: ");
-                scanf("%s", data);
+                scanf("%s", data);//string to be searched
                 printf("%d\n", StrList_count(myList, data));
                 break;
             case 8:
-                //printf("הכנס מחרוזת למחיקה: ");
-                scanf("%s", data);
+                scanf("%s", data);//string to be deleted
                 StrList_remove(myList, data);
                 break;
             case 9:
-                //printf("הכנס אינדקס למחיקה: ");
-                scanf("%d", &index);
+                scanf("%d", &index);//index of string to be removed
                 StrList_removeAt(myList, index);
                 break;
             case 10:
-                StrList_reverse(myList);
-                //printf("הרשימה הופכה בהצלחה.\n");
+                StrList_reverse(myList);//calling reverse function
                 break;
             case 11:
-                StrList_free(myList);
-                myList = StrList_alloc();
-                //printf("הרשימה נמחקה בהצלחה.\n");
+                StrList_free(myList);//deleting current list
+                myList = StrList_alloc();//and creating a new empty one
                 break;
             case 12:
-                StrList_sort(myList);
-                //printf("הרשימה סודרה לפי סדר לקסיקוגרפי.\n");
+                StrList_sort(myList);//calling lexicographic sorting function
                 break;
             case 13:
-                if (StrList_isSorted(myList)) {
+                if (StrList_isSorted(myList)) {//simple condition
                     printf("true\n");
                 } else {
                     printf("false\n");
                 }
                 break;
             case 0:
-                //printf("יוצא מהתוכנית.\n");
-                StrList_free(myList);
+                StrList_free(myList);//exiting the program, therfore freeing allocated memory
                 return 0;
             default:
                 break;
